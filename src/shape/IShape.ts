8dec4160628f9cd32663efaaedb98ef19e038c90
shalldie <394229785@qq.com>
2017-06-27 16:15:46 +0800

    完成Shape

---
+++ src/shape/IShape.ts
@@ -0,0 +1,39 @@
+interface IShape {
+
+    /**
+     * x 坐标
+     * 
+     * @type {number}@memberof IShape
+     */
+    x: number,
+
+    /**
+     * y 坐标
+     * 
+     * @type {number}@memberof IShape
+     */
+    y: number,
+
+    /**
+     * 宽度
+     * 
+     * @type {number}@memberof IShape
+     */
+    width: number,
+
+    /**
+     * 高度
+     * 
+     * @type {number}@memberof IShape
+     */
+    height: number,
+
+    /**
+     * 比例
+     * 
+     * @type {number}@memberof IShape
+     */
+    scale: number
+};
+
+export default IShape;
--- src/shape/Shape.ts
+++ src/shape/Shape.ts
@@ -1,10 +1,12 @@
+import IShape from './IShape';
+
 /**
  * 形状类，基类
  * 
  * @abstract
  * @class Shape
  */
-abstract class Shape {
+abstract class Shape implements IShape {
 
     /**
      * 是否 生存/可用
@@ -44,10 +46,21 @@ abstract class Shape {
      */
     protected ifXSpread: boolean = true;
 
+    /**
+     * 雪碧图数量
+     * 
+     * @protected
+     * @type {number}@memberof Shape
+     */
     protected imgSum: number = 1;
 
     private _x: number = 0;
 
+    /**
+     * 获取/设置 x轴坐标
+     * 
+     * @type {number}@memberof Shape
+     */
     public get x(): number {
         return this._x;
     }
@@ -58,6 +71,11 @@ abstract class Shape {
 
     private _y: number = 0;
 
+    /**
+     * 获取/设置 y轴坐标
+     * 
+     * @type {number}@memberof Shape
+     */
     public get y(): number {
         return this._y;
     }
@@ -68,6 +86,11 @@ abstract class Shape {
 
     private _width: number = 0;
 
+    /**
+     * 获取/设置 宽度
+     * 
+     * @type {number}@memberof Shape
+     */
     public get width(): number {
         return this._width;
     }
@@ -78,6 +101,11 @@ abstract class Shape {
 
     private _height: number = 0;
 
+    /**
+     * 获取/设置 高度
+     * 
+     * @type {number}@memberof Shape
+     */
     public get height(): number {
         return this._height;
     }
@@ -86,8 +114,16 @@ abstract class Shape {
         this._height = ~~hei;
     }
 
+    public scale: number = 1;
+
     private _realWidth: number;
 
+
+    /**
+     * 获取/设置 碰撞检测宽度
+     * 
+     * @type {number}@memberof Shape
+     */
     public get realWidth(): number {
         return this._realWidth;
     }
@@ -98,12 +134,53 @@ abstract class Shape {
 
     private _realHeight: number;
 
+    /**
+     * 获取/设置 碰撞检测高度
+     * 
+     * @type {number}@memberof Shape
+     */
     public get realHeight(): number {
         return this._realHeight;
     }
 
     public set realHeight(realHei: number) {
         this._realHeight = ~~realHei;
+    }
+
+    /**
+     * 透明度
+     * 
+     * @type {number}@memberof Shape
+     */
+    public opacity: number = 1;
+
+    private opacityTimer: number;
+
+    /**
+     * 持续一段时间的半透明
+     * 
+     * @param {number} opacity 半透明度
+     * @param {number} last 持续时间
+     * @memberof Shape
+     */
+    public lastOpacity(opacity: number, last: number): void {
+        this.opacity = opacity;
+        clearTimeout(this.opacityTimer);
+        this.opacityTimer = setTimeout(() => {
+            this.opacity = 1;
+        }, last);
+    }
+
+    constructor({ x, y, width, height, scale }: IShape) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.scale = scale;
+    }
+
+    public onPaint(ctx: CanvasRenderingContext2D): void {
+        if (!this.alive) return;
     }
 }
 
